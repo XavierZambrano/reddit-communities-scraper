78a62229c6e92047017d99e30c46e94d2c1bd330
XavierZambrano <xavierzamco@gmail.com>
2024-06-23 23:45:29 -0600

    feat: parse data

--- reddit_communities_scraper/spiders/communities.py
+++ reddit_communities_scraper/spiders/communities.py
@@ -4,7 +4,22 @@ import scrapy
 class CommunitiesSpider(scrapy.Spider):
     name = "communities"
     allowed_domains = ["reddit.com"]
-    start_urls = ["https://reddit.com"]
+    start_urls = ["https://www.reddit.com/best/communities/1/"]
 
     def parse(self, response):
-        pass
+        containers = response.xpath('//div[@class="community-list"]/div')
+
+        for container in containers:
+            category = container.xpath('//div[@class="community-list"]/div/div/h6/text()').get().strip()
+            subreddit = container.xpath('.//a/text()').get().strip()
+            yield {
+                'url': f'https://www.reddit.com/{subreddit}/',
+                'subreddit': subreddit,
+                'image': container.xpath('.//faceplate-img/@src').get(),
+                'category': category,
+                'members': container.xpath('.//faceplate-number/@number').get(),
+                'description': container.xpath('./@data-public-description-text').get()
+            }
+
+        for url in response.xpath('//div[@id="directory-pagination"]/a/@href').getall():
+            yield response.follow(url, self.parse)
